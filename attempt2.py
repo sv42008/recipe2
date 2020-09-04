a574baca9728f29677fc3f18a7b9b65e10e6b717
sv42008 <70526667+sv42008@users.noreply.github.com>
2020-09-04 11:50:04 +0100

    Update attempt2.py

--- attempt2.py
+++ attempt2.py
@@ -48,63 +48,33 @@ class Recipe(Food):
         self.mealtype = mealtype
         self.rating = rating
         self.servings = servings
-        self.calperserving = float
+        self.calperserving = 0.0
         self.append_to_list(self, list_Recipe)
         self.ingredients = []
-        
+         
        
-        
-
     def create_recipe(self, List_ingreds: list, List_amounts: list):
         self.ingredients.insert(0, List_ingreds)
         self.ingredients.insert(1, List_amounts)
-            
-        # check if everything is working correctly
-        for Ingred in self.ingredients[0]:
-            print(Ingred.name)
-        for int in self.ingredients[1]:
-            print(int)
-        print (self.calperserving)
-        
+
+         #  loop to add total calories in recipe
+        for aa in range(len(self.ingredients)):
+            self.calperserving += (self.ingredients[0][aa].cal/100 * self.ingredients[1][aa])
+        self.calperserving = self.calperserving/self.servings
 
     def update_rating(self, ratingnew : int):
         self.rating = ratingnew
-         #  calculating the calories per serving
-        for aa in range(len(self.ingredients)):
-            print (len(self.ingredients))
-            self.calperserving = float
-            print(self.ingredients[[0], [aa]].cal)
-            # self.calperserving += (self.ingredients[[0], [aa]].cal/100 * self.ingredients[[1], [aa]])
-                #  sum (each ingredients in cal per gram x amount in grams listed in recipe)
-        print(self.calperserving)
-    # def update_priority(self):
-    #   a = self.rating
-    
-
+        
 
 # ---- FUNCTIONS -----
 
 
 
 # ---- COMMAND LINE -----
-# breakfast_cookbook = Cookbook("Breakfast")
-# lunch_cookbook = Cookbook("Lunch")
-# dinner_cookbook = Cookbook("Dinner")
-
-
 
 ketchup = Ingred("Ketchup", 50, 200)
 banana = Ingred("Banana", 200, 200)
-# ketchup.append_to(list_Ingred)
-# banana.append_to(list_Ingred)
 
-for Ingred in list_Ingred:
-    print (Ingred.name)
-
-banana.remove_ingred()
-
-for Ingred in list_Ingred:
-    print (Ingred.name)
 
 BananaKetchup = Recipe("Banana and Ketchup Sabzi", 230, "Breakfast", 0, 2, 0)
 BK = [banana, ketchup]
